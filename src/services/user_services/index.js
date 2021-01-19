78994f53f1d2d5e9616db51acc3f6237a1c232ed
Thayalan Thayaruban <65121546+Thayaruban@users.noreply.github.com>
2021-01-19 10:49:51 +0530

    Update index.js

--- src/services/user_services/index.js
+++ src/services/user_services/index.js
@@ -1,15 +1,45 @@
 const sequelize = require("../../helpers/sequelizer");
 
 const getUsers = async (req, res, next) => {
+
   // sequalize logic goes here
+  const users = await user.findAll();
   next();
 };
 const getUserById = async (req, res, next) => {
   // sequalize logic goes here
+  const userId = await user.findAll({
+    where: {
+      id: req.params.id
+    }
+  });
   next();
 };
 const createUser = async (req, res, next) => {
   // sequalize logic goes here
+  const id = req.body.id,
+  nic = req.body.nic,
+  f_n = req.body.full_name,
+  l_n= req.body.last_name,
+  email=req.body.email,
+  password=req.body.password,
+  mob=req.body.mobile,
+  level=req.body.level;
+     
+  // Create a new complaint and save to DB 
+const newUser = await user.create({id:id,
+  nic:nic,
+  fullname:f_n,
+  last_name:l_n,
+  email:email,
+  password:password,
+  mobile:mob,
+  level:level,
+  created_on:Date.now(),
+  status:0
+
+
+});
   next();
 };
 
