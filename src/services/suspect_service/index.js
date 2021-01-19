8c4fd355af6ba0131569aea533000b1bd56066e1
Thayalan Thayaruban <65121546+Thayaruban@users.noreply.github.com>
2021-01-19 10:48:59 +0530

    Update index.js

--- src/services/suspect_service/index.js
+++ src/services/suspect_service/index.js
@@ -2,22 +2,64 @@ const sequelize = require("../../helpers/sequelizer");
 
 const getSuspects = async (req, res, next) => {
   // sequalize logic here
+  const suspects = await suspect.findAll();
+  
+
   next();
 };
 const getSuspectById = async (req, res, next) => {
   // sequalize logic here
+  const suspectId = await suspect.findAll({
+    where: {
+      id: req.params.id
+    }
+  });
+  
   next();
 };
 const createSuspect = async (req, res, next) => {
   // sequalize logic here
-  next();
+ const id=req.params.id,
+  fn = req.body.first_name,
+  ln = req.body.last_name,
+  desc = req.body.description,
+  img=req.body.image,
+  police = req.user.id;
+  
+// Create a new complaint and save to DB
+  
+const newSuspect = await suspect.create({id:id,
+  first_name:fn,
+  last_name:ln,
+  description:desc,
+  image:img,
+  police:police,
+  status:0
+  
+  
+
+
+});
+  
+  
 };
 const updateSuspect = async (req, res, next) => {
+
   // sequalize logic here
+  const updatedSuspect= await suspect.update({ first_name:req.body.first_name,last_name:req.body.last_name,description:req.body.mobile,address:req.body.description,image:req.body.image }, {
+    where: {
+      id: req.params.id
+    }
+  });
   next();
 };
 const deleteSuspect = async (req, res, next) => {
   // sequalize logic here
+  await law.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
   next();
 };
 
