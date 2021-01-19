f399f9e23f6edff6eabb18754191d91f499dd23a
Thayalan Thayaruban <65121546+Thayaruban@users.noreply.github.com>
2021-01-19 10:48:07 +0530

    Update index.js

--- src/services/law_service/index.js
+++ src/services/law_service/index.js
@@ -6,26 +6,60 @@ const getArticles = async (req, res, next) => {
 };
 const getLaws = async (req, res, next) => {
   // logic to get all laws
+  const laws = await law.findAll();
+  
+  
   next();
 };
 const getLawById = async (req, res, next) => {
   // logic to get law by id
+  const lawId = await law.findAll({
+    where: {
+      id: req.params.id
+    }
+  });
+ 
   next();
 };
 const createLaw = async (req, res, next) => {
   // create a new law
-  next();
+const id = req.body.id,
+   num = req.body.number,
+   law = req.body.law,
+  lawyer=req.user.id;
+   
+
+const newLaw = await law.create({id:id,
+  number:num,
+  law:law,
+  lawyer:lawyer
+  
+});
+  // Create a new law and save to DB
+next();
+  
 };
 const updateLaw = async (req, res, next) => {
   // update low
+  const updatedLaw = await law.update({ number:req.body.number,law:req.body.law }, {
+    where: {
+      id: req.params.id
+    }
+  });
   next();
 };
 const deleteLaw = async (req, res, next) => {
   // delte a law
+  await law.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
   next();
 };
 const getEmergency = async (req, res, next) => {
   // get emergency numbers
+
   next();
 };
 
@@ -38,3 +72,4 @@ module.exports = {
   updateLaw,
   deleteLaw,
 };
+
