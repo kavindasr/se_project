f4f9a03006faed31b8f8275ceb907e5c4caa54a9
Thayalan Thayaruban <65121546+Thayaruban@users.noreply.github.com>
2021-01-21 22:15:13 +0530

    Update apiHome.js

--- src/api/apiHome.js
+++ src/api/apiHome.js
@@ -1,78 +1,95 @@
 const router = require("express").Router();
 const { getSuspects, getSuspectById } = require("../services/suspect_service");
-const auth = require("../services/authService");
+const  auth  = require("../services/authService");
 const {
+  
   getArticles,
   getLaws,
   getLawById,
-  getEmergency,
+  getEmergency
 } = require("../services/law_service");
 const user = require("../models/userModel");
-const bcrypt = require("bcrypt");
+const bcrypt =require("bcrypt");
+
 
 router.post("/login", auth.login);
+  
 
-router.post("/register", (req, res) => {
+
+
+router.post('/register', async(req, res) => {
   console.log("Trying to sign up");
-  const { email, password } = req.body;
-
-  if (!email || !password) {
+  const { email,password } = req.body;
+  
+  if ( !email || !password ) {
     res.send("Please enter all fields");
   }
+  
 
   if (password.length < 6) {
-    res.send("Password must be at least 6 characters");
-  } else {
-    user.findAll({ where: { email: email } }).then((e) => {
-      if (user) {
-        console.log(e);
+     res.send('Password must be at least 6 characters');
+  }
 
-        res.status(400).json({
-          status: "error",
-          error: "email already exist",
-        });
-      } else {
-        const nic = req.body.nic;
-        const f_n = req.body.first_name;
-        const l_n = req.body.last_name;
-        const email = req.body.email;
-        const password = req.body.password;
-        const mob = req.body.mobile;
-        const level = req.body.level;
+ 
+  else {
+    const newUser = await user.findAll({where:{ email: email }});
+    
+    if (newUser.length >0){ 
+      
+      return res.json({
+      status:"error",
+      error:"email already exist"
+    });
 
-        // Create a new user and save to DB
-        const newUser = user.build({
-          nic: nic,
-          first_name: f_n,
-          last_name: l_n,
-          email: email,
-          password: password,
-          mobile: mob,
-          level: level,
-          created_on: Date.now(),
-          status: 0,
-        });
+  }else {
+        
+        const nic = req.body.nic,
+              f_n = req.body.first_name,
+              l_n= req.body.last_name,
+              email=req.body.email,
+              password=req.body.password,
+              mob=req.body.mobile,
+              level=req.body.level;
+              
+     
+  // Create a new user and save to DB 
+        const newUser = await user.build({
+          nic:nic,
+          first_name:f_n,
+          last_name:l_n,
+          email:email,
+          password:password,
+          mobile:mob,
+          level:level,
+          created_on:Date.now(),
+          status:0
+        })
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
             if (err) throw err;
             newUser.password = hash;
             newUser
               .save()
-              .then((user) => {
+              .then(user => {
                 res.status(200).json({
-                  status: "success",
-                  message: "Successfully added ",
+                  status:"success",
+                  message:"Successfully added "
                 });
-
+                
                 //res.redirect('/users/login');
               })
-              .catch((err) => console.log(err));
+              .catch(err => console.log(err));
           });
         });
-      }
-    });
+
+
+}
   }
+    
+  
 });
+  
+
 
 router.get("/", getArticles, (req, res, next) => {
   res.send(req.arr);
