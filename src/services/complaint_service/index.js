85f1365db77a4883a6763a4c2b961f0dd98b9a01
MusabMahmoodh <musabmah70707@gmail.com>
2021-01-23 10:19:25 +0530

    creation problem

--- src/services/complaint_service/index.js
+++ src/services/complaint_service/index.js
@@ -3,7 +3,6 @@ const complaint = require("../../models/complaintModel");
 const getComplaints = async (req, res, next) => {
   // logic for getting all complaints
   const complaints = await complaint.findAll();
-  
 
   next();
 };
@@ -11,53 +10,64 @@ const getComplaintById = async (req, res, next) => {
   // logic for getting a complaint by id
   const complaintId = await complaint.findAll({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   });
-  
+
   next();
 };
 const createComplaint = async (req, res, next) => {
   // create new complaint
   const id = req.body.id,
-  nic = req.body.nic,
-  f_n = req.body.full_name,
-  mob=req.body.mobile,
-  add = req.body.address,
-  file=req.body.file,
-  comp=req.body.complaint,
-  people=req.user.id;
-     
-  // Create a new complaint and save to DB 
-  const newComplaint = await complaint.create({id:id,
-  nic:nic,
-  fullname:f_n,
-  mobile:mob,
-  address:add,
-  complaint:comp,
-  file:file,
-  people_id:people,
-  created_on:Date.now(),
-  status:0
+    nic = req.body.nic,
+    f_n = req.body.full_name,
+    mob = req.body.mobile,
+    add = req.body.address,
+    file = req.body.file,
+    comp = req.body.complaint,
+    people = req.user.id;
+
+  // Create a new complaint and save to DB
+  const newComplaint = await complaint.create({
+    id: id,
+    nic: nic,
+    fullname: f_n,
+    mobile: mob,
+    address: add,
+    complaint: comp,
+    file: file,
+    people_id: people,
+    created_on: Date.now(),
+    status: 0,
   });
   next();
-  
 };
 const updateComplaint = async (req, res, next) => {
   // update complaint logic
- const updatedComplaint= await complaint.update({ nic:req.body.nic,fullname:req.body.fullname,mobile:req.body.mobile,address:req.body.address,complaint:req.body.compalint ,file:req.body.file}, {
-    where: {
-      id: req.params.id
+  const updatedComplaint = await complaint.update(
+    {
+      nic: req.body.nic,
+      fullname: req.body.fullname,
+      mobile: req.body.mobile,
+      address: req.body.address,
+      complaint: req.body.compalint,
+      file: req.body.file,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
     }
-  });
+  );
+  
   next();
 };
 const deleteComplaint = async (req, res, next) => {
   // delete complaint logic
   await complaint.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   });
   next();
 };
--- src/services/user_services/index.js
+++ src/services/user_services/index.js
@@ -2,48 +2,45 @@ const sequelize = require("../../helpers/sequelizer");
 const user = require("../../models/userModel");
 
 const getUsers = async (req, res, next) => {
-
   // sequalize logic goes here
   const users = await user.findAll();
-  req.users=users;
+  req.users = users;
   next();
-  
 };
 const getUserById = async (req, res, next) => {
   // sequalize logic goes here
   const userId = await user.findAll({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   });
-  res.locals.userId=userId;
+  res.locals.userId = userId;
   next();
 };
 const createUser = async (req, res, next) => {
   // sequalize logic goes here
   const id = req.body.id,
-  nic = req.body.nic,
-  f_n = req.body.first_name,
-  l_n= req.body.last_name,
-  email=req.body.email,
-  password=req.body.password,
-  mob=req.body.mobile,
-  level=req.body.level;
-     
-  // Create a new user and save to DB 
-const newUser = await user.create({id:id,
-  nic:nic,
-  fullname:f_n,
-  last_name:l_n,
-  email:email,
-  password:password,
-  mobile:mob,
-  level:level,
-  created_on:Date.now(),
-  status:0
+    nic = req.body.nic,
+    f_n = req.body.first_name,
+    l_n = req.body.last_name,
+    email = req.body.email,
+    password = req.body.password,
+    mob = req.body.mobile,
+    level = req.body.level;
 
-
-})
+  // Create a new user and save to DB
+  const newUser = await user.create({
+    id: id,
+    nic: nic,
+    first_name: f_n,
+    last_name: l_n,
+    email: email,
+    password: password,
+    mobile: mob,
+    level: level,
+    created_on: Date.now(),
+    status: 0,
+  });
   next();
 };
 
